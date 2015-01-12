46c04a52396170b913327dda2b65e382ee4ac6b9
Hendry Luk <hendrymail@gmail.com>
2015-01-13 08:49:20 +1100

    Added a failing injection test

---
+++ src/Alpaca.Weld.Test/InjectTest.cs
@@ -0,0 +1,33 @@
+﻿using Alpaca.Weld.Attributes;
+using Alpaca.Weld.Core;
+using NUnit.Framework;
+
+namespace Alpaca.Weld.Test
+{
+    public class InjectTest
+    {
+        public interface IRepository<T>
+        {
+        }
+
+        public class Repository<T>
+        {
+        }
+
+        public class Target
+        {
+            [Inject] public IRepository<int> _repo;
+        }
+
+        [Test]
+        public void TestGenericInjection()
+        {
+            var catalog = new WeldCatalog();
+            catalog.RegisterComponent(typeof(Repository<>));
+            catalog.RegisterComponent(typeof(Target));
+            catalog.RegisterInject(typeof(Target).GetField("_repo"));
+
+            new WeldEngine(catalog).Run();
+        }
+    }
+}
